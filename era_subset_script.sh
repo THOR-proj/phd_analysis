407f3f1883a39935aa4a5bdae76667ab4f943267
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2021-12-09 11:28:37 +1100

    era5 subset script amended

--- era_subset_script.sh
+++ era_subset_script.sh
@@ -16,29 +16,29 @@ for year in $(seq 1998 2016); do
         echo "Subsetting ${var}$. \n"
 
         echo "Subsetting January. \n"
-        subset year 01 31 var
+        subset ${year} 01 31 ${var}
 
         echo "Subsetting February. \n"
         echo '2000, 2004, 2008, 2012, 2016' | grep -q ${year}
         if [[$? -eq 0]; then
-            subset year 02 29 var
+            subset ${year} 02 29 ${var}
         else
-            subset year 02 28 var
+            subset ${year} 02 28 ${var}
         fi
 
         echo "Subsetting March. \n"
-        subset year 03 31 var
+        subset ${year} 03 31 ${var}
 
         echo "Subsetting April."
-        subset year 04 31 var
+        subset ${year} 04 31 ${var}
 
         echo "Subsetting October. \n"
-        subset year 10 31 var
+        subset ${year} 10 31 ${var}
 
         echo "Subsetting November. \n"
-        subset year 11 30 var
+        subset ${year} 11 30 ${var}
 
         echo "Subsetting December. \n"
-        subset year 12 31 var
+        subset ${year} 12 31 ${var}
     done
 done
