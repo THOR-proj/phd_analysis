49a0ca74e8210050af984a390bf31c158526fef8
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2021-12-13 13:07:27 +1100

    fixed tracks_script

--- CPOL_tracks_script.py
+++ CPOL_tracks_script.py
@@ -1,7 +1,21 @@
 import CPOL_func as cf
 import sys
+import argparse
 
 sys.path.insert(0, '/home/563/esh563/TINT')
 
-for year in list(set(range(1998, 2016)) - {2007, 2008}):
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    '--years', '-y', type=str, default='',
+    help='years to get tracks for')
+
+args = parser.parse_args()
+if args.years == '':
+    years = sorted(list(set(range(1998, 2016)) - {2007, 2008}))
+else:
+    years = args.years
+    years = list(map(int, years.split(',')))
+
+for year in years:
+    print('Getting year {}'.format(year))
     cf.get_CPOL_season(year)
