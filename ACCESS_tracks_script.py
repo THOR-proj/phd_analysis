89db0b7df7c155a837544eadb4aed7ebd8efddc8
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2022-06-14 11:01:26 +1000

    back

---
+++ ACCESS_tracks_script.py
@@ -0,0 +1,15 @@
+import CPOL_func as cf
+import sys
+import argparse
+
+sys.path.insert(0, '/home/563/esh563/TINT')
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    '--radar', '-r', type=str, default='63',
+    help='radar domain to get tracks for')
+
+args = parser.parse_args()
+
+print('Getting radar {}'.format(args.radar))
+cf.get_ACCESS_season(args.radar)
