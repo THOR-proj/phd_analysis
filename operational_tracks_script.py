d28970f7de305680b87da89c19f247952fe42ffd
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2022-06-17 14:24:18 +1000

    back

--- operational_tracks_script.py
+++ operational_tracks_script.py
@@ -6,19 +6,19 @@ sys.path.insert(0, '/home/563/esh563/TINT')
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
-    '--year', '-y', type=str, default=2020,
+    '--year', '-y', type=int, default=2020,
     help='year to get tracks for')
 
 parser.add_argument(
-    '--month', '-m', type=str, default=12,
+    '--month', '-m', type=int, default=12,
     help='month to get tracks for')
 
 parser.add_argument(
-    '--radar', '-r', type=str, default=63,
-    help='years to get tracks for')
+    '--radar', '-r', type=int, default=63,
+    help='radar to get tracks for')
 
 args = parser.parse_args()
 
 print('Getting year {}, month {}, radar {}.'.format(
     args.year, args.month, args.radar))
-cf.get_CPOL_season(args.radar, year=args.year, month=args.month)
+cf.get_oper_month(args.radar, year=args.year, month=args.month)
