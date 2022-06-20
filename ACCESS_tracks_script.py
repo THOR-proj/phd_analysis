0b88984ba5ad94fa92a1949e16b7390def1a9458
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2022-06-20 15:09:52 +1000

    ACCESS_remote_test

--- ACCESS_tracks_script.py
+++ ACCESS_tracks_script.py
@@ -8,8 +8,11 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
     '--radar', '-r', type=str, default='63',
     help='radar domain to get tracks for')
+parser.add_argument(
+    '--year', '-y', type=str, default='63',
+    help='radar domain to get tracks for')
 
 args = parser.parse_args()
 
-print('Getting radar {}'.format(args.radar))
-cf.get_ACCESS_season(args.radar)
+print('Getting radar {}, year {}'.format(args.radar, args.year))
+cf.get_ACCESS_season(args.radar, args.year)
