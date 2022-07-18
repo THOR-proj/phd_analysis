afcf882210e135957630755dec4a02ad0978f793
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2022-07-18 16:46:35 +1000

    backup

--- operational_scans_script.py
+++ operational_scans_script.py
@@ -9,10 +9,6 @@ parser.add_argument(
     help='year to generate verification scans for')
 
 parser.add_argument(
-    '--month', '-m', type=int, default=10,
-    help='month to generate verification scans for')
-
-parser.add_argument(
     '--radar', '-r', type=int, default=63,
     help='radar region to generate verification scans for')
 
@@ -21,6 +17,13 @@ args = parser.parse_args()
 save_dir = '/g/data/w40/esh563/TINT_tracks/'
 fig_dir = '/g/data/w40/esh563/TINT_figures/'
 
+years_months = [
+    [args.year, 10], [args.year, 11], [args.year, 12],
+    [args.year+1, 1], [args.year+1, 2], [args.year+1, 3],
+    [args.year+1, 4]]
+
 print('Getting year {}'.format(args.year))
-cf.gen_operational_verification_figures(
-    save_dir, fig_dir, radar=args.radar, year=args.year, month=args.month)
+for i in range(len(years_months)):
+    cf.gen_operational_verification_figures(
+        save_dir, fig_dir, radar=args.radar,
+        year=years_months[i][0], month=years_months[i][1])
