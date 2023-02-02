b32288db07d64f00b595ffc87eb6f909184b0b1a
Ewan Short <shorte1@6250l-148376-l.mobility.unimelb.net.au>
2023-02-02 11:48:26 +1100

    args

--- operational_monsoon_scans_script.py
+++ operational_monsoon_scans_script.py
@@ -43,7 +43,7 @@ else:
 
 print('Getting year {}'.format(args.year))
 cf.gen_operational_verification_figures(
-    args.out, args.figures, radar=args.radar,
+    args.tracks, args.figures, radar=args.radar,
     year=args.year, month=args.month, exclusions=['simple_duration_cond'],
     suffix='_{}_{}'.format(args.year, args.month),
     start_date=start_date, end_date=end_date)
